44604ec2cbd9b1fe5195aeab5b9df61f8411b155
gianluca-petri <103224731+gianluca-petri@users.noreply.github.com>
2022-04-28 21:47:16 -0300

    Update EjemploTipos.cpp

--- DD-EjemploTipos/EjemploTipos.cpp
+++ DD-EjemploTipos/EjemploTipos.cpp
@@ -14,17 +14,19 @@ int main() {    // Defino la clase principal del programa por donde C++ ingresa,
 
     const bool booleano = true;                          // Defino cte booleana con valor true
     const char caraSimple = 'z';                         // Defino cte caracter con valor z.
-    const unsigned enteroSinSigno = 654;                 // Defino cte entera sin signo con valor 654.
-    const int entero = -123;                             // Defino cte entera con valor -123.
-    const double doblePres = 0.1414;                     // Defino cte double de punto flotante con doble precision
     const std::string palabra = "Esto es un string";     // Defino cte cadena de caracteres
+    const int entero = -123;                             // Defino cte entera con valor -123.
+    const unsigned enteroSinSigno = 654;                 // Defino cte entera sin signo con valor 654
+    const double doblePres = 0.1414;                     // Defino cte double de punto flotante con doble precision
+    
 
 
     assert(booleano);                                    // Asevero si cte booleana es verdadera.
     assert(caraSimple == 'z');                           // Asevero si cte caracter es z.
+    assert(17 <= palabra.length());                      // Asevero si cte cadena de caracteres tiene 12 caracteres de largo.
+    assert(entero != 123)                                // Asevero si cte entera es distinta de 123.
     assert(enteroSinSigno != 456);                       // Asevero si cte entera sin signo es distinta a 456
     assert(doblePres >= entero);                         // Asevero si cte de punto flotante con doble precision es mayor o igual a cte entera
-    assert(17 <= palabra.length());                      // Asevero si cte cadena de caracteres tiene 12 caracteres de largo.
 
     std::cout << "Todo funciona bien!\n";   // Si lo aseverado es correcto retorna 0 
 
